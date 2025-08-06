d03736ef35ec892e5869c3e3f9c151d101d979c7
Jonas Melo <jonasFNMelo@gmail.com>
2025-08-06 14:32:38 -0300

    first commit

---
+++ CoreDataMedium/CoreDataMedium/CoreDataMediumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMediumApp.swift
+//  CoreDataMedium
+//
+//  Created by Jonas Fernando Nascimento Melo on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMediumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
